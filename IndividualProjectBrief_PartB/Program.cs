0bc6878dfc92a8c187099deec08458e4121ea798
George Kalokyris <kalokirisg@gmail.com>
2021-04-23 16:01:08 +0100

    Add project files.

---
+++ IndividualProjectBrief_PartB/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndividualProjectBrief_PartB
+{
+    class Manager
+    {
+       
+        using (IndividualProjectBrief_Part_BEntities Context = new IndividualProjectBrief_Part_BEntities())
+        {
+        
+        
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
