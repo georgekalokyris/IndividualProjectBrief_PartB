19c313afc40e220ec80e8cc83f471ca5385ae15e
George Kalokyris <kalokirisg@gmail.com>
2021-04-29 21:24:51 +0100

    First fully working - customisation tbd

--- IndividualProjectBrief_PartB/Program.cs
+++ IndividualProjectBrief_PartB/Program.cs
@@ -44,11 +44,91 @@ namespace IndividualProjectBrief_PartB
                         TuitionFees = fees
                     };
 
+                    var courses = Reader.GetAllCourses().Select(x => new SelectItem(x.CourseId, x.Title)).ToList(); ;
+
+                    
+
+                    Console.WriteLine("Please select a course to assign the student to");
+
+                    Program.Print(courses);
+
+                    var courseid = Convert.ToInt32(Console.ReadLine());
+    
+                    CoursesStudents tocourse = new CoursesStudents()
+                    {
+                        CourseId = courseid,
+                        StudentId = student.StudentId
+                    };
+
+                    Context.CoursesStudents.Add(tocourse);
                     Context.Students.Add(student);
+
+                    var assignments = Context.Assignments.Select(x => new {x.AssignmentId, x.Title, x.Description, x.Courses })
+                                            .Where(f => f.Courses.CourseId == courseid)
+                                            .ToList();
+
+                    var course = Reader.GetAllCourses().Where(f => f.CourseId == courseid).First();
+                    Console.WriteLine("Press 1 to assign any assignment of the course to the student or x to continue");
+                    Console.WriteLine($"Assignments of {course}");
+                    Program.Print(assignments.Select(x => $"{x.AssignmentId}, {x.Title}")); 
+
+
+                    var opt = Console.ReadLine();
+
+
+                    if (opt == "1")
+                    {
+                        bool more = true;
+                        while (more)
+                        {
+                            Console.WriteLine("Please provide the id of the assignment, to assign to the student");
+                            var assignmentid = int.Parse(Console.ReadLine());
+
+                            var assignmentTitle = Context.Assignments.FirstOrDefault(c => c.AssignmentId == assignmentid).Title;
+
+                            var assignmentDescription = Context.Assignments.FirstOrDefault(c => c.AssignmentId == assignmentid).Description;
+                         
+                            var assignmentSubDate = Context.Assignments.FirstOrDefault(c => c.AssignmentId == assignmentid).SubDateTime;
+
+                            var assignmentOralMark = Context.Assignments.FirstOrDefault(c => c.AssignmentId == assignmentid).OralMark;
+                            
+                            var assignmentTotalMark = Context.Assignments.FirstOrDefault(c => c.AssignmentId == assignmentid).TotalMark;
+
+                            var cs = new Assignments
+                            {
+                                Title = assignmentTitle,
+                                Description = assignmentDescription,
+                                SubDateTime = assignmentSubDate,
+                                OralMark = assignmentOralMark,
+                                TotalMark = assignmentTotalMark,
+                                CourseId = courseid,
+                                StudentId = student.StudentId
+                            };
+
+                            Context.Assignments.Add(cs);
+
+                            
+                            Console.WriteLine("Press any button to continue adding assignments or x to return to the previous menu");
+
+                            var x = Console.ReadLine();
+                            if (x == "x")
+                            {
+                                break;
+                            }
+                            else continue;
+                        }
+                        
+                    }
+                    else continue;
+
+                   
                     Context.SaveChanges();
 
-                    Console.WriteLine("\nRecord added: ");
+                    Console.WriteLine("\nStudent added: ");
                     Console.WriteLine(student);
+                    Console.WriteLine("to Course:");
+                    Console.WriteLine(course);
+                    
                     Console.ForegroundColor = ConsoleColor.Green;
 
                     Console.WriteLine("\n\nPress enter to continue adding records or 'x' to return to the top menu");
@@ -272,7 +352,7 @@ namespace IndividualProjectBrief_PartB
                     Console.WriteLine("\nTrainer added: ");
                     Console.WriteLine(trainer);
 
-                    Console.WriteLine("\nto course: ");
+                    Console.WriteLine("\nto Course: ");
                     Console.WriteLine(courses.First(c=>c.Id == courseid));
 
                     Console.ForegroundColor = ConsoleColor.Green;
@@ -618,16 +698,14 @@ namespace IndividualProjectBrief_PartB
 
     class SelectItem
     {
+        public int Id { get; }
+
+        public string Value { get; }
         public SelectItem(int id, string value)
         {
             Id = id;
             Value = value;
         }
-
-        public int Id { get; }
-
-        public string Value { get; }
-
         public override string ToString()
         {
             return $"{Id} - {Value}";
